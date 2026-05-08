fe3f5936f13e67c35ca030b442250a4ccf9d32bb
João Pedro Chaves <joaopedrochavescontato@gmail.com>
2026-05-08 13:41:07 -0300

    refactor: modularize SQL codebase into separate schema creation, data insertion, and demonstration scripts

---
+++ source/Code/Script_Query_Demo.sql
@@ -0,0 +1,13 @@
+/*
+SQL script to demonstrate the functionality of the database and tables for the supermarket project.
+SQL Script para demonstrar o funcionamento do banco de dados e das tabelas para o projeto de supermercado.
+
+Author: João Pedro Vieira da Costa Chaves
+Date: 2026/05/08
+Version: 1.0.0
+
+About the project: [https://github.com/joaopedro-chaves/database_supermercado_projeto_UniFECAF]
+
+Project created for the discipline of Database Systems of UniFECAF.
+Projeto criado para a disciplina de Banco de Dados do UniFECAF
+*/
---
+++ source/Code/Script_Query_Insert.sql
@@ -0,0 +1,13 @@
+/*
+SQL script to insert sample data into the database.
+SQL Script para inserção de dados de exemplo no banco de dados.
+
+Author: João Pedro Vieira da Costa Chaves
+Date: 2026/05/08
+Version: 1.0.0
+
+About the project: [https://github.com/joaopedro-chaves/database_supermercado_projeto_UniFECAF]
+
+Project created for the discipline of Database Systems of UniFECAF.
+Projeto criado para a disciplina de Banco de Dados do UniFECAF
+*/
