7037ce08e61e3c57878abb575f29f5be128866b8
João Pedro Chaves <joaopedrochavescontato@gmail.com>
2026-05-10 02:59:06 -0300

    refactor: rename lote_produto to lote_estoque in schema and update documentation diagrams

--- source/Code/Script_Query_Create.sql
+++ source/Code/Script_Query_Create.sql
@@ -18,29 +18,29 @@ USE db_supermercado;
 
 CREATE TABLE tbl_colaboradores (
     id_colaboradores INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nome_colaborador VARCHAR(255) NULL,
-    funcao_colaborador VARCHAR(45) NULL,
-    data_contratacao DATE NULL,
-    email_colaborador VARCHAR(255) NULL UNIQUE,
-    cpf_colaborador CHAR(11) NULL UNIQUE,
-    fk_id_supervisor INT NULL,
-    -- Referência para evitar recursividade
+    nome_colaborador VARCHAR(255) NOT NULL,
+    funcao_colaborador VARCHAR(45) NOT NULL,
+    data_contratacao DATE NOT NULL,
+    email_colaborador VARCHAR(255) NOT NULL UNIQUE,
+    cpf_colaborador CHAR(11) NOT NULL UNIQUE,
+    fk_id_supervisor INT NOT NULL,
+    -- Chave estrangeira para evitar recursividade
     CONSTRAINT fk_colaborador_supervisor FOREIGN KEY (fk_id_supervisor) REFERENCES tbl_colaboradores (id_colaboradores)
 );
 
 CREATE TABLE tbl_produto (
     id_produto INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nome_produto VARCHAR(255) NULL,
-    tipo_produto VARCHAR(45) NULL,
-    valor_produto DECIMAL(10, 2) NULL,
-    quantidade_produto INT NOT NULL,
-    categoria_produto VARCHAR(45) NULL
+    nome_produto VARCHAR(255) NOT NULL,
+    tipo_produto VARCHAR(45) NOT NULL,
+    valor_produto DECIMAL(10, 2) NOT NULL,
+    quantidade_produto INT NOT NOT NULL,
+    categoria_produto VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE tbl_estoque (
     id_estoque INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     data_validade DATE NOT NULL,
-    lote_produto VARCHAR(45) NOT NULL,
+    lote_estoque VARCHAR(45) NOT NULL,
     quantidade_estoque INT NOT NULL,
     data_entrada DATE NOT NULL,
     fk_tbl_produto_id_produto INT NOT NULL,
@@ -49,33 +49,33 @@ CREATE TABLE tbl_estoque (
 
 CREATE TABLE tbl_fidelizacao (
     id_fidelizacao INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    data_cadastro DATE NULL,
+    data_cadastro DATE NOT NULL,
     saldo_pontos INT NOT NULL DEFAULT 0,
     nivel_fidelizacao INT NOT NULL DEFAULT 1,
-    data_ultima_atualizacao DATE NULL,
-    preferencias_categoria VARCHAR(45) NULL
+    data_ultima_atualizacao DATE NOT NULL,
+    preferencias_categoria VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE tbl_clientes (
     id_clientes INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nome_cliente VARCHAR(255) NULL,
-    cpf_cliente CHAR(11) NULL UNIQUE,
-    num_cel_cliente VARCHAR(12) NULL UNIQUE,
-    email_cliente VARCHAR(255) NULL UNIQUE,
-    tbl_fidelizacao_id_fidelizacao INT NOT NULL,
-    CONSTRAINT fk_clientes_to_fidelizacao FOREIGN KEY (
-        tbl_fidelizacao_id_fidelizacao
-    ) REFERENCES tbl_fidelizacao (id_fidelizacao)
-);
+    nome_cliente VARCHAR(255) NOT NULL,
+    cpf_cliente CHAR(11) NOT NULL UNIQUE,
+    num_cel_cliente VARCHAR(12) NOT NULL UNIQUE,
+    email_cliente VARCHAR(255) NOT NULL UNIQUE,
+    tbl_fidelizacao_id_fidelizacao INT, -- O cliente pode ou não ter um programa de fidelização, definido pela regra de negócio.
+    CONSTRAINT fk_clientes_to_fidelizacao FOREIGN KEY (tbl_fidelizacao_id_fidelizacao) REFERENCES tbl_fidelizacao (id_fidelizacao)
+
+-- Verifica se os valores de CPF são válidos
+CHECK (cpf_cliente ~ '^[0-9]{11}$') );
 
 CREATE TABLE tbl_endereco_colaborador (
     id_endereco_colaborador INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    rua VARCHAR(45) NOT NULL,
-    bairro VARCHAR(45) NOT NULL,
-    cidade VARCHAR(45) NOT NULL,
-    pais VARCHAR(45) NOT NULL,
-    cep VARCHAR(9) NOT NULL,
-    fk_tbl_colaboradores_id_colaboradores INT NOT NULL,
+    rua_colaborador VARCHAR(45) NOT NULL,
+    bairro_colaborador VARCHAR(45) NOT NULL,
+    cidade_colaborador VARCHAR(45) NOT NULL,
+    pais_colaborador VARCHAR(45) NOT NULL,
+    cep_colaborador VARCHAR(9) NOT NULL,
+    fk_tbl_colaboradores_id_colaboradores INT NOT NULL UNIQUE,
     CONSTRAINT fk_endereco_colaborador_to_colaboradores FOREIGN KEY (
         fk_tbl_colaboradores_id_colaboradores
     ) REFERENCES tbl_colaboradores (id_colaboradores)
@@ -83,37 +83,39 @@ CREATE TABLE tbl_endereco_colaborador (
 
 CREATE TABLE tbl_endereco_cliente (
     id_endereco_cliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    rua VARCHAR(45) NOT NULL,
-    bairro VARCHAR(45) NOT NULL,
-    cidade VARCHAR(45) NOT NULL,
-    pais VARCHAR(45) NOT NULL,
-    cep VARCHAR(9) NOT NULL,
-    fk_tbl_clientes_id_clientes INT NOT NULL,
+    rua_cliente VARCHAR(45) NOT NULL,
+    bairro_cliente VARCHAR(45) NOT NULL,
+    cidade_cliente VARCHAR(45) NOT NULL,
+    pais_cliente VARCHAR(45) NOT NULL,
+    cep_cliente VARCHAR(9) NOT NULL,
+    fk_tbl_clientes_id_clientes INT NOT NULL UNIQUE,
     CONSTRAINT fk_endereco_cliente_to_clientes FOREIGN KEY (fk_tbl_clientes_id_clientes) REFERENCES tbl_clientes (id_clientes)
 );
 
 CREATE TABLE tbl_vendas (
     id_vendas INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    valor_total DECIMAL(10, 2) NULL,
-    datahora_venda DATETIME NULL,
-    comissao_venda DECIMAL(10, 2) NULL,
+    valor_total DECIMAL(10, 2) NOT NULL,
+    datahora_venda DATETIME NOT NULL,
+    comissao_venda DECIMAL(10, 2) NOT NULL,
     numero_venda VARCHAR(45) NOT NULL UNIQUE,
-    forma_pagamento VARCHAR(45) NULL,
+    forma_pagamento VARCHAR(45) NOT NULL,
     fk_tbl_colaboradores_id_colaboradores INT NOT NULL,
     fk_tbl_clientes_id_clientes INT NOT NULL,
     CONSTRAINT fk_vendas_to_colaboradores FOREIGN KEY (
         fk_tbl_colaboradores_id_colaboradores
     ) REFERENCES tbl_colaboradores (id_colaboradores),
     CONSTRAINT fk_vendas_to_clientes FOREIGN KEY (fk_tbl_clientes_id_clientes) REFERENCES tbl_clientes (id_clientes)
-);
+
+-- Verifica se os valores são válidos ou positivos.
+CHECK (valor_total > 0 AND comissao_venda > 0) );
 
 CREATE TABLE tbl_historico_pontos (
     id_historico_pontos INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    entrada_pontos INT NULL,
-    saida_pontos INT NULL,
+    entrada_pontos INT NOT NULL,
+    saida_pontos INT NOT NULL,
     data_hora_pontos DATETIME NOT NULL,
-    fk_tbl_vendas_id_vendas INT NOT NULL,
-    fk_tbl_fidelizacao_id_fidelizacao INT NOT NULL,
+    fk_tbl_vendas_id_vendas INT NULL,
+    fk_tbl_fidelizacao_id_fidelizacao INT NULL,
     CONSTRAINT fk_historico_pontos_to_vendas FOREIGN KEY (fk_tbl_vendas_id_vendas) REFERENCES tbl_vendas (id_vendas),
     CONSTRAINT fk_historico_pontos_to_fidelizacao FOREIGN KEY (
         fk_tbl_fidelizacao_id_fidelizacao
@@ -123,9 +125,11 @@ CREATE TABLE tbl_historico_pontos (
 CREATE TABLE tbl_produto_venda (
     id_produto_venda INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     quantidade_vendida INT NOT NULL,
-    valor_total_itens DECIMAL(10, 2) NULL,
+    valor_total_itens DECIMAL(10, 2) NOT NULL,
     fk_tbl_produto_id_produto INT NOT NULL,
     fk_tbl_vendas_id_vendas INT NOT NULL,
     CONSTRAINT fk_produto_venda_to_produto FOREIGN KEY (fk_tbl_produto_id_produto) REFERENCES tbl_produto (id_produto),
     CONSTRAINT fk_produto_venda_to_vendas FOREIGN KEY (fk_tbl_vendas_id_vendas) REFERENCES tbl_vendas (id_vendas)
-);
+
+-- Verifica se os valores são válidos ou positivos.
+CHECK (quantidade_vendida > 0 AND valor_total_itens > 0) );
